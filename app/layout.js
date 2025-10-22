fd9144628c16b655c5b8d2cb33ceb2043025c69a
ChakraVaishnav <guntakachakravaishnavreddy@gmail.com>
2025-10-22 10:38:28 +0530

    Adsterra ads added

--- app/layout.js
+++ app/layout.js
@@ -89,11 +89,13 @@ export default function RootLayout({ children }) {
         <meta name="twitter:title" content={metadata.twitter.title} />
         <meta name="twitter:description" content={metadata.twitter.description} />
         <meta name="twitter:image" content={metadata.twitter.images[0]} />
+  <script type='text/javascript' src='//pl27901593.effectivegatecpm.com/e3/25/a4/e325a46cf2a1eeb4b33181c8cd52ff37.js'></script>
       </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
+          <script type='text/javascript' src='//pl27901593.effectivegatecpm.com/e3/25/a4/e325a46cf2a1eeb4b33181c8cd52ff37.js'></script>
         {children}
       </body>
     </html>
